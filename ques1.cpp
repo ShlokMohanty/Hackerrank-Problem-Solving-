cf93c7e713d8f751d7e1fcb83d759f477f858f20
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-02-03 23:38:52 +0530

    tried to resolve the query on vectors
    in this i tried to help my freind the find out the errors and improve the code.

--- ques1.cpp
+++ ques1.cpp
@@ -1,38 +1,18 @@
-//Given an array of integers, find the sum of its elements.
-
-For example, if the array , , so return .
-
-Function Description
-
-Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
-
-simpleArraySum has the following parameter(s):
-
-ar: an array of integers
-Input Format
-
-The first line contains an integer, , denoting the size of the array.
-The second line contains  space-separated integers representing the array's elements.
-//
-
-
-#include <bits/stdc++.h>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-int simpleArraySum(int sum)
+vector<int> vec;
+int simpleSumArray(int sum)
 {
-    vector<int>::iterator i = vec.begin();
+    vector<int>:: iterator i = vec.begin();
     return sum;
 }
 int main()
 {
-    cout<< "This is a program in c++ to print the sum of two arrays using  stl library";
-    vector<int> vec;
-    int i, sum=0 ;
+    cout<<"this is a program to print the sum of two arrays using stl cpp"<<endl;
+    int i,sum=0;
     for(i=0;i<10;i++)
     {
-        vec.push_back();      
+        vec.push_back();
     }
-int simpleArraySum(int sum);    
-cout<< "The sum of the array is:"<<simpleArraySum(int sum) ;  
+    cout<<"the sum of the array is :"<<simpleSumArray(sum)<<endl;
 }
