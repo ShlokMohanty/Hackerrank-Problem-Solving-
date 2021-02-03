0b3933b5a2f7b2762e4ddee09e16230df710bdd3
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-03 23:16:46 +0530

    Create ques1.cpp

---
+++ ques1.cpp
@@ -0,0 +1,38 @@
+//Given an array of integers, find the sum of its elements.
+
+For example, if the array , , so return .
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers representing the array's elements.
+//
+
+
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+int simpleArraySum(int sum)
+{
+    vector<int>::iterator i = vec.begin();
+    return sum;
+}
+int main()
+{
+    cout<< "This is a program in c++ to print the sum of two arrays using  stl library";
+    vector<int> vec;
+    int i, sum=0 ;
+    for(i=0;i<10;i++)
+    {
+        vec.push_back();      
+    }
+int simpleArraySum(int sum);    
+cout<< "The sum of the array is:"<<simpleArraySum(int sum) ;  
+}
